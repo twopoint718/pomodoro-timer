e7b36af5de4f4da1e3b4ae59d3353f29cd7081e6
Chris Wilson <cwilson@8thlight.com>
2022-01-04 21:30:37 -0600

    WIP: display driver

--- Core/Inc/pcd8544.h
+++ Core/Inc/pcd8544.h
@@ -2,13 +2,20 @@
  * pcd8544.h
  *
  *  Created on: Jan 2, 2022
- *      Author: cjw
+ *      Author: Adafruit
  */
 
 #ifndef INC_PCD8544_H_
 #define INC_PCD8544_H_
 
+#define WIDTH 84  // 84 pixels wide
+#define HEIGHT 48 // 48 pixels high
 
-void LCD_Init(SPI_HandleTypeDef *, uint16_t);
+#define PCD8544_FUNCTIONSET 	0x20
+#define PCD8544_DISPLAYCONTROL	0x08
+#define PCD8544_DISPLAYNORMAL	0x04
+
+void LCD_Init(SPI_HandleTypeDef *);
+void Command(SPI_HandleTypeDef *, uint8_t);
 
 #endif /* INC_PCD8544_H_ */
--- Core/Src/pcd8544.c
+++ Core/Src/pcd8544.c
@@ -16,20 +16,31 @@
  * @param  GPIO_Pin specifies the port bit to be written.
  *         This parameter can be any combination of GPIO_Pin_x where x can be
  * (0..15).
- * @param  PinState specifies the value to be written to the selected bit.
- *         This parameter can be one of the GPIO_PinState enum values:
- *            @arg GPIO_PIN_RESET: to clear the port pin
- *            @arg GPIO_PIN_SET: to set the port pin
  * @retval None
  */
-void LCD_Init(SPI_HandleTypeDef *spi_handle, uint16_t reset_pin)
+void LCD_Init(SPI_HandleTypeDef *hspi)
 {
 	// SPI must already be initialized
-	assert_param(HAL_SPI_GetState(spi_handle) == HAL_SPI_STATE_READY);
+	assert_param(HAL_SPI_GetState(hspi) == HAL_SPI_STATE_READY);
 
-	// Toggle reset pin low, then high
-	HAL_GPIO_WritePin(GPIOA, reset_pin, GPIO_PIN_RESET);
-	// TODO: small delay
-	HAL_GPIO_WritePin(GPIOA, reset_pin, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(LCD_RESET_GPIO_Port, LCD_RESET_Pin, GPIO_PIN_RESET); // set reset pin low
+	HAL_Delay(1);                                        // delay
+	HAL_GPIO_WritePin(LCD_RESET_GPIO_Port, LCD_RESET_Pin, GPIO_PIN_SET);   // set high
 
+	// normal mode
+	Command(hspi, PCD8544_FUNCTIONSET);
+
+	// Set display to Normal
+	Command(hspi, PCD8544_DISPLAYCONTROL | PCD8544_DISPLAYNORMAL);
+}
+
+/**
+ * @brief Send a general SPI command to the LCD display
+ * @param command is the single-byte SPI command
+ * @param dc_pin is the pin conected to Data/Command pin of the PCD8544
+ * @retval None
+ */
+void Command(SPI_HandleTypeDef *hspi, uint8_t command) {
+	HAL_GPIO_WritePin(LCD_DC_GPIO_Port, LCD_DC_Pin, GPIO_PIN_RESET);
+	HAL_SPI_Transmit(hspi, &command, 1, 0);
 }
