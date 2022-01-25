c1f7bade4995863cd0f3fda4e24df2526b06a6ba
Chris Wilson <cwilson@8thlight.com>
2022-01-24 23:48:26 -0600

    WIP: enable I2C2
    - Looks like some of my custom code got bulldozed, plz check on it
    
      🐍 no step on snek!

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -41,6 +41,7 @@ void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOA, LCD_RESET_Pin|LCD_DC_Pin, GPIO_PIN_RESET);
