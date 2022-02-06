984d577df31952bfadc66d6184f9944a0d233aca
Chris Wilson <cwilson@8thlight.com>
2022-02-05 21:07:30 -0600

    Remove USB support/middleware 🔌

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,7 +21,6 @@
 #include "i2c.h"
 #include "spi.h"
 #include "usart.h"
-#include "usb_device.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -91,7 +90,6 @@ int main(void)
   MX_SPI1_Init();
   MX_I2C2_Init();
   MX_UART4_Init();
-  MX_USB_DEVICE_Init();
   /* USER CODE BEGIN 2 */
   BSP_ACCELERO_Init();
 
