2ffcd626ea7298bfb2cc12ff10c073ddb2e6a60b
Chris Wilson <cwilson@8thlight.com>
2022-02-16 21:05:15 -0600

    Enable reloading the timer preload value ♻️

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -45,7 +45,7 @@ void MX_TIM2_Init(void)
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim2.Init.Period = 4294967295;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
   if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
   {
     Error_Handler();
