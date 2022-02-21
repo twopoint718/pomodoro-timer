8ef078cc529b24db1845d3174277da1113671085
Chris Wilson <cwilson@8thlight.com>
2022-02-20 19:05:03 -0600

    And generate the actual EXT interrupt handler 😓
    - Need to check the box in the config tool to generate
      `EXTI15_10_IRQHandler` which in turn calls
      `HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_13)`

--- Core/Inc/stm32l4xx_it.h
+++ Core/Inc/stm32l4xx_it.h
@@ -57,6 +57,7 @@ void PendSV_Handler(void);
 void SysTick_Handler(void);
 void TIM2_IRQHandler(void);
 void I2C2_EV_IRQHandler(void);
+void EXTI15_10_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
