396373c4082928d8ccf431e8586449721b87bff9
Chris Wilson <cwilson@8thlight.com>
2022-03-10 22:57:06 -0600

    Add headless build script 🎃

---
+++ Core/Src/pomodoro.c
@@ -0,0 +1,1 @@
+../../model/pomodoro.c
---
+++ build.sh
@@ -0,0 +1,6 @@
+/Applications/STM32CubeIDE.app/Contents/MacOS/STM32CubeIDE \
+	--launcher.suppressErrors \
+	-nosplash \
+	-application org.eclipse.cdt.managedbuilder.core.headlessbuild \
+	-build pomodoro-timer \
+	-data /Users/cjw/code/cube_ide
