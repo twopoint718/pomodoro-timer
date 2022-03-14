de2d00aac2684dc64b97f13812ac79d3c93a7ddd
Chris Wilson <cwilson@8thlight.com>
2022-03-13 21:15:34 -0500

    WIP: makefile 🗒

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -31,6 +31,7 @@ extern "C" {
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include <stdint.h>
 #include <string.h>
 #include <stdbool.h>
 #include <stdlib.h>
